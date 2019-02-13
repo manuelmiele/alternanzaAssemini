9707a51949191e24c2dcec7f467093ba2390209d
william <willy.pinna@hotmail.it>
2019-02-13 11:54:47 +0100

    Classi java Ordine, Articolo e classe delle eccezzioni: CtrlException

---
+++ nuovoNegozio/src/main/java/com/journaldev/spring/model/CtrlException.java
@@ -0,0 +1,14 @@
+package org.alternanza.prova.classi;
+
+public class CtrlException extends Exception{
+
+		public CtrlException() {
+			super();
+		}
+		
+		public CtrlException(String msg) {
+			super(msg);
+		}
+	}
+
+
